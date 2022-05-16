a016bcc5bebe6be3f15f38f03e0c920c079b9cb5
perezcatriel <perezcatriel@gmail.com>
2022-05-16 11:22:16 -0300

    Categories y refactor a axios

--- src/main.js
+++ src/main.js
@@ -1,25 +1,60 @@
+const api = axios.create({
+	baseURL: "https://api.themoviedb.org/3/",
+	headers: {
+		"Content-Type": "application/json;charset=utf-8",
+	},
+	params: {
+		api_key: API_KEY,
+	},
+});
+
 async function getTredingMoviesPreview() {
-	const res = await fetch(
-		`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
-	);
-	const data = await res.json();
+	const { data } = await api(`trending/movie/day`);
 
 	const movies = data.results;
-	movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+	movies.forEach((movie) => {
+		const trendingPreviewMoviesContainer = document.querySelector(
+			"#trendingPreview .trendingPreview-movieList"
+		);
 
-    const movieContainer = document.createElement('div');
-    movieContainer.classList.add('movie-container');
+		const movieContainer = document.createElement("div");
+		movieContainer.classList.add("movie-container");
 
-    const movieImg = document.createElement('img');
-    movieImg.classList.add('movie-img');
-    movieImg.setAttribute('alt', movie.title);
-    movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path,);
+		const movieImg = document.createElement("img");
+		movieImg.classList.add("movie-img");
+		movieImg.setAttribute("alt", movie.title);
+		movieImg.setAttribute(
+			"src",
+			"https://image.tmdb.org/t/p/w300" + movie.poster_path
+		);
 
+		movieContainer.append(movieImg);
+		trendingPreviewMoviesContainer.append(movieContainer);
+	});
+}
 
-    movieContainer.append(movieImg);
-    trendingPreviewMoviesContainer.append(movieContainer);
-  })
+async function getCategoriesPreview() {
+	const { data } = await api(`genre/movie/list`);
+
+	const categories = data.genres;
+	categories.forEach((category) => {
+		const previewCategoriesMoviesContainer = document.querySelector(
+			".categoriesPreview-list"
+		);
+
+		const categoryContainer = document.createElement("div");
+		categoryContainer.classList.add("category-container");
+
+		const categoryTitle = document.createElement("h3");
+		categoryTitle.classList.add("category-title");
+		categoryTitle.setAttribute("id", "id" + category.id);
+		const categoryTitleText = document.createTextNode(category.name);
+
+		categoryTitle.append(categoryTitleText);
+		categoryContainer.append(categoryTitle);
+		previewCategoriesMoviesContainer.append(categoryContainer);
+	});
 }
 
 getTredingMoviesPreview();
+getCategoriesPreview();
